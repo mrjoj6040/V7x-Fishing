3b2cb58c26c566c75e63d9f4df88de0ba3fd1968
No-Name-404 <56244233+No-Name-404@users.noreply.github.com>
2019-10-09 17:51:39 +0200

    clear

--- start.py
+++ start.py
@@ -46,6 +46,7 @@ B#|/       \\_______/\\_______)|/     \\|\\_______/|/    )_)(_______)
 
 class main(Gr):
 	def __init__(self):
+        os.system('clear')
 		try:
 			print (self.list_main())
 			self.Functions()
